2d2d5b40d8d97525f6724f9fd92a933b212b97f0
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-10-19 03:22:27 +0000

    changed q5

--- ch14hw/q5.c
+++ ch14hw/q5.c
@@ -3,7 +3,7 @@
 
 int main() {
     int *data = (int*) malloc(100 * sizeof(int)); // allocate memory for 100 integers
-    data[99] = 0; // set the last element to zero
+    data[100] = 0; // set the last element to zero
     // do something with the array
     free(data); // free the memory
     return 0;
