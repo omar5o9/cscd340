0f374754f64af73d30a47faeeaa7725894c2f475
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-11-17 04:06:14 +0000

    q3 again

--- ch29hw/q3.c
+++ ch29hw/q3.c
@@ -4,76 +4,68 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#define MAX_THREADS 64
+#define ARRAY_SIZE 1000000
 
-int thread_count;
-long long n;
-double threshold;
-int counter = 0;
-pthread_mutex_t mutex;
+int array[ARRAY_SIZE];
+int count = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void* Thread_sum(void* rank);
+struct thread_args {
+    int threshold;
+};
 
-int main(int argc, char* argv[]) {
-    long thread;
-    pthread_t* thread_handles;
-    struct timeval start, end;
-    double elapsed_time;
+void *count_elements(void *arg) {
+    struct thread_args *args = (struct thread_args *) arg;
+    int local_count = 0;
 
-    thread_count = strtol(argv[1], NULL, 10);
-    n = strtoll(argv[2], NULL, 10);
-    threshold = strtod(argv[3], NULL);
-
-    thread_handles = malloc(thread_count * sizeof(pthread_t));
-    pthread_mutex_init(&mutex, NULL);
-
-    gettimeofday(&start, NULL);
-    for (thread = 0; thread < thread_count; thread++) {
-        pthread_create(&thread_handles[thread], NULL, Thread_sum, (void*) thread);
-    }
-
-    for (thread = 0; thread < thread_count; thread++) {
-        pthread_join(thread_handles[thread], NULL);
-    }
-    gettimeofday(&end, NULL);
-
-    elapsed_time = (end.tv_sec - start.tv_sec) * 1000.0;
-    elapsed_time += (end.tv_usec - start.tv_usec) / 1000.0;
-
-    printf("With n = %lld terms,\n", n);
-    printf("    Our estimate of pi = %f\n", 4.0 * counter / ((double) n));
-    printf("    Time elapsed = %f milliseconds\n", elapsed_time);
-    printf("    Counter = %d\n", counter);
-
-    pthread_mutex_destroy(&mutex);
-    free(thread_handles);
-    return 0;
-}
-
-void* Thread_sum(void* rank) {
-    long my_rank = (long) rank;
-    long long i;
-    long long my_n = n / thread_count;
-    long long my_first_i = my_n * my_rank;
-    long long my_last_i = my_first_i + my_n;
-
-    int my_count = 0;
-    double factor;
-    if (my_first_i % 2 == 0) {
-        factor = 1.0;
-    } else {
-        factor = -1.0;
-    }
-
-    for (i = my_first_i; i < my_last_i; i++, factor = -factor) {
-        if (factor * 1.0 / (2 * i + 1) > threshold) {
-            my_count++;
+    for (int i = 0; i < ARRAY_SIZE / (int) sizeof(array) / (int) sizeof(int); i++) {
+        if (array[i] > args->threshold) {
+            local_count++;
         }
     }
 
     pthread_mutex_lock(&mutex);
-    counter += my_count;
+    count += local_count;
     pthread_mutex_unlock(&mutex);
 
-    return NULL;
+    pthread_exit(NULL);
+}
+
+int main() {
+    int num_threads, threshold;
+    pthread_t threads[num_threads];
+    struct thread_args args;
+    struct timeval start, end;
+    double elapsed_time;
+
+    printf("Enter the number of threads: ");
+    scanf("%d", &num_threads);
+
+    printf("Enter the threshold value: ");
+    scanf("%d", &threshold);
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = rand() % 100;
+    }
+
+    args.threshold = threshold;
+
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&threads[i], NULL, count_elements, (void *) &args);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    gettimeofday(&end, NULL);
+
+    elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+
+    printf("Total count: %d\n", count);
+    printf("Elapsed time: %f seconds\n", elapsed_time);
+
+    return 0;
 }
