bee7ecb7f6837b6541256137b4500e3f7ff97a69
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-11-29 02:55:08 +0000

    q2 edit

--- ch39hw/q2.c
+++ ch39hw/q2.c
@@ -5,28 +5,36 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void listFiles(const char* directory, int detailed) {
-    DIR* dir = opendir(directory);
-    if (dir == NULL) {
-        printf("Error opening directory: %s\n", directory);
+void printFileInfo(const char* filename) {
+    struct stat fileStat;
+    if (stat(filename, &fileStat) == -1) {
+        perror("stat");
         return;
     }
 
+    printf("File: %s\n", filename);
+    printf("Owner: %d\n", fileStat.st_uid);
+    printf("Group: %d\n", fileStat.st_gid);
+    printf("Permissions: %o\n", fileStat.st_mode & 0777);
+    printf("Size: %ld bytes\n", fileStat.st_size);
+    printf("Last modified: %s", ctime(&fileStat.st_mtime));
+    printf("\n");
+}
+
+void listFiles(const char* directory, int showFileInfo) {
+    DIR* dir;
     struct dirent* entry;
+
+    if ((dir = opendir(directory)) == NULL) {
+        perror("opendir");
+        return;
+    }
+
     while ((entry = readdir(dir)) != NULL) {
-        if (detailed) {
-            struct stat fileStat;
-            char filePath[256];
-            snprintf(filePath, sizeof(filePath), "%s/%s", directory, entry->d_name);
-            if (stat(filePath, &fileStat) == -1) {
-                printf("Error getting file information: %s\n", entry->d_name);
-                continue;
-            }
-            printf("File: %s\n", entry->d_name);
-            printf("Owner: %d\n", fileStat.st_uid);
-            printf("Group: %d\n", fileStat.st_gid);
-            printf("Permissions: %o\n", fileStat.st_mode & 0777);
-            // Print other file information as needed
+        if (showFileInfo) {
+            char path[PATH_MAX];
+            snprintf(path, sizeof(path), "%s/%s", directory, entry->d_name);
+            printFileInfo(path);
         } else {
             printf("%s\n", entry->d_name);
         }
@@ -36,22 +44,18 @@ void listFiles(const char* directory, int detailed) {
 }
 
 int main(int argc, char* argv[]) {
-    int detailed = 0;
     const char* directory;
 
-    if (argc == 2 && strcmp(argv[1], "-l") == 0) {
-        detailed = 1;
+    if (argc == 1) {
         directory = getcwd(NULL, 0);
     } else if (argc == 3 && strcmp(argv[1], "-l") == 0) {
-        detailed = 1;
         directory = argv[2];
-    } else if (argc == 2) {
-        directory = argv[1];
     } else {
-        directory = getcwd(NULL, 0);
+        printf("Usage: %s [-l] [directory]\n", argv[0]);
+        return 1;
     }
 
-    listFiles(directory, detailed);
+    listFiles(directory, argc == 3 && strcmp(argv[1], "-l") == 0);
 
     return 0;
 }
